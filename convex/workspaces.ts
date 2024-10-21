5d8bcbb19c2432d8e0773d7d49cbdde38f57e0f7
Polo1121 <michal.jasinski11121@gmail.com>
2024-10-21 11:57:44 +0200

    Add members functionality

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,16 +2,37 @@ import { getAuthUserId } from "@convex-dev/auth/server";
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
+const generateCode = () =>
+  Array.from(
+    { length: 6 },
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+
 export const get = query({
   args: {},
   handler: async (ctx) => {
     const userId = await getAuthUserId(ctx);
 
     if (!userId) {
-      throw new Error("Unauthorized");
+      return [];
     }
 
-    return await ctx.db.query("workspaces").collect();
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((m) => m.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      workspaces.push(workspace);
+    }
+
+    return workspaces;
   },
 });
 
@@ -26,13 +47,21 @@ export const create = mutation({
       throw new Error("Unauthorized");
     }
 
-    const joinCode = "1234";
+    const joinCode = generateCode();
 
-    return await ctx.db.insert("workspaces", {
+    const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
       userId,
       joinCode,
     });
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
+    });
+
+    return workspaceId;
   },
 });
 
@@ -44,7 +73,18 @@ export const getById = query({
     const userId = await getAuthUserId(ctx);
 
     if (!userId) {
-      throw new Error("Unauthorized");
+      return null;
+    }
+
+    const isMember = await ctx.db
+      .query("members")
+      .withIndex("by_user_id_and_workspace_id", (q) =>
+        q.eq("userId", userId).eq("workspaceId", args.id)
+      )
+      .unique();
+
+    if (!isMember) {
+      return null;
     }
 
     return await ctx.db.get(args.id);
